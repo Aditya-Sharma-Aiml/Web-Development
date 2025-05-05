0dd2af084f8ff15c03519f1bc923a83ae2647983
Adita-Sharma-Aiml <adityasharma.cseaiml@gmail.com>
2025-05-06 04:27:10 +0530

    main

---
+++ JavaScript/JSfoundation/part1/printing.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log("Mr.");
+process.stdout.write("Aditya")
+process.stdout.write(" Sharma")
+
+console.table({
+    city:"Ayodhya"
+})
+console.warn({
+    city:"Ayodhya"
+})
