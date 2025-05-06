9e2126765cf40566630a4754b7a70908d7150cad
Adita-Sharma-Aiml <adityasharma.cseaiml@gmail.com>
2025-05-07 02:07:52 +0530

    On branch main 	modified:   JavaScript/JSfoundation/hello.js 	new file:   JavaScript/JSfoundation/part1/assignment.js 	modified:   JavaScript/JSfoundation/part1/changes.js 	new file:   JavaScript/JSfoundation/part1/logical.js 	new file:   JavaScript/JSfoundation/part1/operations.js 	new file:   JavaScript/JSfoundation/part1/operatorPrecedence.js 	modified:   JavaScript/JSfoundation/hello.js 	modified:   JavaScript/JSfoundation/part1/changes.js

--- JavaScript/JSfoundation/hello.js
+++ JavaScript/JSfoundation/hello.js
@@ -1,2 +1,2 @@
-console.log("Hello Aditya!");
+console.log("Hello Aditya");
 
---
+++ JavaScript/JSfoundation/part1/changes.js
@@ -0,0 +1,9 @@
+let gameName = "spiderman";
+gameName = "batman";
+console.log(gameName);
+
+const username = "aadi_unique"
+//TypeError: Assignment to constant variable.
+//username  = "Aditya" 
+console.log(username);
+
