0c283861699a0563f4e5c4826b56e32af5f620ab
Adita-Sharma-Aiml <adityasharma.cseaiml@gmail.com>
2025-05-07 05:15:55 +0530

    On branch main 	new file:   JavaScript/JSfoundation/part2/dateTime.js 	modified:   JavaScript/JSfoundation/part2/nonPrimitiveDataType.js

--- JavaScript/JSfoundation/part2/nonPrimitiveDataType.js
+++ JavaScript/JSfoundation/part2/nonPrimitiveDataType.js
@@ -3,12 +3,13 @@ let obj = {};
 console.log(obj);
 console.log(typeof obj);
 
-const username = { // username : object is fixed but inside this *
-    firstname : "Aditya",  // *-> these primitives can be changed
-    lastname : "Sharma",
-    Email : "abc@gmail.com",
-    isLoggedin : true,
-    "Ph_no" : "12335536" //**specific
+const username = {
+  // username : object is fixed but inside this *
+  firstname: "Aditya", // *-> these primitives can be changed
+  lastname: "Sharma",
+  Email: "abc@gmail.com",
+  isLoggedin: true,
+  Ph_no: "12335536", //**specific
 };
 console.log(username);
 console.log(username.firstname);
@@ -21,15 +22,17 @@ console.log(username.firstname);
 username.gender = "male"; // add new primitive
 console.log(username);
 
-console.log(username['Ph_no']); // **specific
+console.log(username["Ph_no"]); // **specific
 console.log(username);
 console.log(typeof username);
 
-//Date
+//Date  : take reference from commented output -> //
+console.log("************date************");
+
 let today = new Date();
-console.log(today);
-console.log(today.getDate()); //  date: 1-31
-console.log(today.getDay());
+console.log(typeof today); //object
+console.log(today); //2025-05-06T23:16:58.016Z
+console.log(today.getDate()); //  date: 1-31 -
 /* sun=1
    mon=2
    .
@@ -37,18 +40,18 @@ console.log(today.getDay());
    .
    sat=3
 */
-
+console.log(today.getDay());
 
 // 2.Arrays : collecction of data
-let heros = ["ironman","spiderman","batman"]
+let heros = ["ironman", "spiderman", "batman"];
 console.log(typeof heros);
 console.log(heros);
 
-let anotheUser  = ["Aditya",true,21]
+let anotheUser = ["Aditya", true, 21];
 console.log(anotheUser[0]);
 
 //(some times javascript work smartly -> assume related to given data : string + might be string )
-console.log(1 + '1'); // out : 11
+console.log(1 + "1"); // out : 11
 console.log("1" + 1); // out : 11
 
 // true = 1 , false = 0
@@ -57,19 +60,9 @@ console.log(Number(isValue)); // implicit conversion
 
 console.log(isValue + 1); //2
 
-let value = "2abc"
+let value = "2abc";
 console.log(Number(value)); //NaN
 console.log(typeof Number(value)); //type of NaN : out ->Number
 
 console.log(Number(null)); //0
 console.log(Number(undefined)); // NaN
-
-
-
-
-
-
-
-
-
-
