b2965b3b7b19927290c53177155c7f360d23bf61
Adita-Sharma-Aiml <adityasharma.cseaiml@gmail.com>
2025-05-07 00:22:11 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   JavaScript/JSfoundation/part1/changes.js
    	modified:   JavaScript/JSfoundation/part1/datatype.js

--- JavaScript/JSfoundation/part1/datatype.js
+++ JavaScript/JSfoundation/part1/datatype.js
@@ -1,1 +1,39 @@
-const a = 2;
+// console.log("comment");
+/*
+multiline 
+comment
+*/
+/*
+
+datatype: 
+1.string
+2.number
+3.boolean
+4.Bigint
+
+other{
+5.undefined
+6.null
+}
+7.object
+8.symbol
+
+*/
+
+var Score = 102; // old and var=> name the storage location
+
+let score = 102;
+let name = "Aditya";
+let isloggedin = true;
+
+let getScore = score;
+
+//object
+let city = ["sultanpur", "delhi", "Noida"];
+let user = {
+  name: "Aditya",
+  age: 21,
+  gender: "male",
+};
+
+console.log(getScore);
